648d658b909a3049d2fa62a15f8c79586ddbd3a5
whatcodehaveyougit <siggywatt@yahoo.com>
2020-02-11 21:58:15 +0000

    fix?

--- helpers/create_router.js
+++ helpers/create_router.js
@@ -1,20 +1,24 @@
-const express = require('express');
+  const express = require('express');
 const ObjectId = require('mongodb').ObjectId;
 
 const createRouter = function(collection) {
   const router = express.Router();
 
-  router.get('/', (req, res) => {
-    collection
-    .find()
-    .toArray()
-    .then((docs) => res.json(docs))
-    .catch((err) => {
-      console.error(err)
-      res.status(500);
-      res.json({ status: 500, error: err })
-    })
-  })
+router.get('/', (req, res) => {
+  res.send();
+});
+
+  // router.get('/', (req, res) => {
+  //   collection
+  //   .find()
+  //   .toArray()
+  //   .then((docs) => res.json(docs))
+  //   .catch((err) => {
+  //     console.error(err)
+  //     res.status(500);
+  //     res.json({ status: 500, error: err })
+  //   })
+  // })
 
   // router.post('/', (req, res) => {
   //   const newData = req.body;
